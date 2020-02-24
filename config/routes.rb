1ea50f45b22ff18708d09b76f59d598dbdbd0569
chris peterson <christonpeterson@gmail.com>
2020-02-24 11:00:23 +0200

    views page for story

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'story/show'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :units, only: [:index, :create, :update]
