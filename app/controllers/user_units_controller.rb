d701d258178dd16ebc83e80dde4e10409eae3f22
Matan Nahmani <matanmatan999new@gmail.com>
2020-02-27 16:31:37 +0200

    ok

--- app/controllers/user_units_controller.rb
+++ app/controllers/user_units_controller.rb
@@ -9,12 +9,12 @@ class UserUnitsController < ApplicationController
             case option
             when 10
               if checkgems(TENROLL)
-                response = {}
+                response = []
                 10.times do |i|
                   result = generate
                   unit = Unit.where(rarity: result.downcase).sample
                   UserUnit.create(unit_id: unit.id, user_id: current_user.id)
-                  response[i.to_s] = unit.name
+                  response << [unit.name,unit.rarity]
                 end
                 current_user.gems -= TENROLL
                   current_user.save!
