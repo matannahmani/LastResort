36a1ceb8373e7435c1d447a20e6bb650c914136e
Matan Nahmani <matanmatan999new@gmail.com>
2020-03-31 07:31:47 +0300

    loader added styling fixes

--- app/javascript/packs/App.jsx
+++ app/javascript/packs/App.jsx
@@ -8,6 +8,7 @@ import PropTypes from 'prop-types'
 import Tabs from 'components/tabs';
 import Myfriends from 'components/myfriends';
 import Searchfriend from 'components/searchfriend';
+import Spinner from 'components/spinner'
 
 class App extends Component {
   request = async (url) =>
@@ -15,11 +16,13 @@ class App extends Component {
       let response = await fetch(`${location.origin}/${url}`);
       let data = await response.json()
       if (data.code === 200 && data.msg === 'Success index'){
-        this.setState({value:0,data: data.friends})
+        this.setState({value:0,data: data.friends,loading: false})
       }
       else if (data.code === 200){
-        console.log(data)
-        this.setState({value:this.state.value,data: data.data})
+        this.setState({value:this.state.value,data: data.data,loading: false})
+      }
+      else{
+        this.setState({loading: false})
       }
     }
     postData = async (url, data) => {
@@ -42,7 +45,8 @@ class App extends Component {
 
   state = {
     data: [],
-    value: 0
+    value: 0,
+    loading: true
   };
   componentDidMount(){
     this.request('/friends');
@@ -50,15 +54,15 @@ class App extends Component {
   getState = (value) => {
     switch (value) {
       case 0:
-        this.setState({value: 0,data: []})
+        this.setState({value: 0,data: [],loading: true})
         this.request('friends');
         break;
       case 1:
-        this.setState({value: 1,data: []});
+        this.setState({value: 1,data: [],loading: true});
         this.request('/friends/randomfriends');
         break;
       case 2:
-        this.setState({value: 2,data: []});
+        this.setState({value: 2,data: [],loading: true});
         this.request('/friends/friendrequests');
     }
   }
@@ -66,24 +70,23 @@ class App extends Component {
     this.request(`/friends/search/${value}`);
   }
   addFriendHandler = (name) => {
-    console.log(name);
     this.postData('/friends/add',{name: name}).then((response) =>
     {
       if (response.code === 200){
         alert(response.data)
-        this.setState({value: this.state.value, data: this.state.data.filter ( (i) => i[1] !== name)})
+        this.setState({value: this.state.value, data: this.state.data.filter ( (i) => i[1] !== name),loading: false})
       }
       else
         alert(response.msg)
+        this.setState({loading: false})
     });
   }
   acceptFriendHandler = (name,status) => {
-    console.log(name,status);
     this.postData('/friends/acceptfriend',{nickname: name,accept: status}).then((response) =>
     {
       if (response.code === 200){
         alert(response.data)
-        this.setState({value: this.state.value, data: this.state.data.filter ( (i) => i[1] !== name)})
+        this.setState({value: this.state.value, data: this.state.data.filter ( (i) => i[1] !== name),loading: false})
       }
       else
         alert(response.msg)
@@ -92,18 +95,20 @@ class App extends Component {
     render () { // Needs to be implemented in class-based components! Needs to return some JSX!
       let person;
       let search = null;
+      let spinner = null;
       if (this.state.data.constructor === Array || this.state.data.constructor === Object )
       person = this.state.data.map((person) => <Myfriends key={person[1]} name={person[1]} acceptFriend={this.acceptFriendHandler}title={person[2].title} addFriend={this.addFriendHandler} value={this.state.value}/>)
       if (this.state.value === 1) {
           search = <Searchfriend sendSearch={this.searchState}/>;
-          console.log(person)
      }
+     this.state.loading ? spinner = <Spinner/> : spinner = null
     return (
         <div>
         <Tabs sendState={this.getState} />
         {search}
         {person}
-        <a href={`${location.origin}/games/main`}>Go back</a>
+        {spinner}
+        <a className="fixed-bottom .text-info btn btn-secondary btn-lg" href={`${location.origin}/games/main`}>Main screen</a>
         </div>
     );
   }
